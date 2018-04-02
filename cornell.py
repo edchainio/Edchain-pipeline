da620f7ee5773b3b1f6d1c16a119195c8cd37fbb
Sandy Maracallo <smaracallo.11@outlook.com>
2018-04-02 16:10:31 -0400

    Update cornell.py

--- cornell.py
+++ cornell.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup as BS
 import re
 import urllib.request, urllib.parse, urllib.error  #url handling modules
-
+import json
 from fixture_builder import FixtureBuilder
 from config_content_builder import ConfigBuilder
 
@@ -13,13 +13,6 @@ class Cornell:
 		self.content_download_urls = []
 		self.new_list = []
 
-	def process(self):
-		# get links to all course home pages
-		#course_page_urls = self.get_course_page_urls()
-		# get product
-		#product = self.get_product()
-
-		content_urls = self.get_course_content()
 	def get_course_name(self,soup):
 		"""
 		Get the name of a specified course.
@@ -56,7 +49,6 @@ class Cornell:
 		return course_image
 
 	def get_image_continue(self, soup_cont):
-		# Get the images of a specified course.
 		course_image = []
 		for link in soup_cont.select('span.field-content img[src]'):
 			course_image.append(link['src'])
@@ -110,22 +102,24 @@ class Cornell:
 		Returns: List of links to all courses.
 		"""
 		course_links =[]
+		root_url = 'http://onlinelearning.cornell.edu'
 		for link in soup.select('span.field-content a[href]'):
-			course_links.append(link['href'])
+			new_url = root_url + link['href']
+			course_links.append(new_url)
 			course_links.append(' \n')
 		
 		self.new_list.append(course_links)
-		#print (course_links)
 		return course_links
 	
 	def get_course_page_urls_continue(self,soup_cont):
 		course_links =[]
+		root_url = 'http://onlinelearning.cornell.edu'
 		for link in soup_cont.select('span.field-content a[href]'):
-			course_links.append(link['href'])
+			new_url = root_url + link['href']
+			course_links.append(new_url)
 			course_links.append(' \n')
 		
 		self.new_list.append(course_links)
-		#print (course_links)
 		return course_links
 
 	def run_all(self):
@@ -147,15 +141,47 @@ class Cornell:
 		self.get_course_description(soup)
 		self.get_course_description_continue(soup_cont)
 
+	
+		print (self.new_list)
 		with open('cornell.txt', 'w+') as wr: # w+: create if file doesnt exist
 			for course_doc in self.new_list:
 				for name in course_doc:	
 					wr.write(name)
-				
-# def main():
-# 	director = Pennstate(FixtureBuilder(), ConfigBuilder())
-# 	director.construct()
+
+	def json_convert(self):
+		view_courses_url = 'http://onlinelearning.cornell.edu/welcome?f%5B0%5D=field_cost%3A98&view-order=grid'
+		view_courses_url_cont = 'http://onlinelearning.cornell.edu/welcome?f%5B0%5D=field_cost%3A98&view-order=grid&page=1'
+		response = requests.get(view_courses_url)
+		response_cont = requests.get(view_courses_url_cont)
+		soup = BS(response.content, "html.parser")
+		soup_cont = BS(response_cont.content, "html.parser")
+
+		new_dict = zip(self.get_course_name(soup),
+		#self.get_course_name_continue(soup_cont),
+		self.get_course_page_urls(soup),
+		#self.get_course_page_urls_continue(soup_cont),
+		self.get_organization(soup),
+		#self.get_organization_continue(soup_cont),
+		self.get_image(soup),
+		#self.get_image_continue(soup_cont),
+		self.get_course_description(soup))
+		#self.get_course_description_continue(soup_cont))
+
+		new_dict = list(new_dict)
+		keys = ['content_title', 'copyright_img_link', 'course_Organization', 'course_urls','course_Description']
+		Cornell_course_list = []
+		Cornell_course = {}
+		
+		for i in new_dict[::2]:
+			i = list(i)
+			mix_list = zip(keys,i)
+			mix_list = list(mix_list)
+			Cornell_course_list.append(dict(mix_list))
+		Cornell_course["attribution"] = Cornell_course_list
+		
+		with open('cornell_resources.json', 'w+') as wr: 
+			wr.write(json.dumps(Cornell_course))
 
 if __name__ == '__main__':
-	#get_course_name(soup)
-	Cornell().run_all()
+	Cornell().json_convert()
+	#Cornell().run_all()
